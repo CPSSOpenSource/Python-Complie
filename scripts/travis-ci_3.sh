75ce7955a68b90a167431768d2a2f000445ad1c0
cpssnetwork <cpss.network@gmail.com>
2018-04-27 18:00:52 +0900

    Initial commit

--- scripts/travis-ci_3.sh
+++ scripts/travis-ci_3.sh
@@ -5,9 +5,9 @@ do
   for index in $(seq 5)
   do
     cd ..
-    PATH="$TRAVIS_BUILD_DIR/${!user}/$WEEK/level_$index.py"
-    echo -e "compliing: $FILE_PATH"
-    python $FILE_PATH
+    CURRENT=$TRAVIS_BUILD_DIR/${!user}/$WEEK/level_$index.py
+    echo -e "compliing: $CURRENT\n"
+    python $CURRENT
   done
   echo -e "finished: ${!user}"
 done
