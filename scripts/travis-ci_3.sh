155972cf0879ed6ec35caaa996172b1c3422dfdc
cpssnetwork <cpss.network@gmail.com>
2018-04-27 17:37:31 +0900

    initial commit

---
+++ scripts/travis-ci_3.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+user_array=("CSB" "LMK" "KSB")
+for user in ${user_array[@]}
+do
+  for index in $(seq 5)
+  do
+    python "../${!user}/$WEEK/level_$index.py"
+  done
+  echo -e "finished: ${!user}"
+done
